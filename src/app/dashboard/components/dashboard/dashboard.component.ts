08f8967a5992297fda862907d77458595bba6705
MohammedAl-rowad <rowadwo@gmail.com>
2020-09-04 12:41:13 +0300

    init

---
+++ src/app/dashboard/components/dashboard/dashboard.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { DashboardService } from './dashboard.service';
+
+@Component({
+  selector: 'panadora-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
+  providers: [DashboardService],
+})
+export class DashboardComponent implements OnInit {
+  constructor(private readonly dashboardService: DashboardService) {}
+
+  ngOnInit(): void {
+    this.dashboardService.traversePages();
+  }
+}
