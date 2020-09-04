e8984370b2701454b38d93b5bba164876b555fa9
MohammedAl-rowad <rowadwo@gmail.com>
2020-09-04 18:36:39 +0300

    good start

--- Client/src/app/dashboard/components/dashboard/dashboard.service.ts
+++ Client/src/app/dashboard/components/dashboard/dashboard.service.ts
@@ -1,33 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
 @Injectable()
 export class DashboardService {
   constructor(private readonly http: HttpClient) {}
 
   get() {
-    const { API, API_KEY, username } = environment;
-    return this.traversePages(API, this.basicAuth(username, API_KEY));
+    const { API } = environment;
+    return this.traversePages(API);
   }
 
-  private traversePages(API: string, headers: { headers: HttpHeaders }) {
+  private traversePages(API: string) {
     const data = [];
     const { ceil } = Math;
     console.log;
-    return this.http
-      .get(
-        `${API}/search?jql=assignee=5a69b5b8031a591456af4307&maxResults=100`,
-        { ...headers }
-      )
-      .subscribe({
-        next(response) {
-          console.log(ceil);
-        },
-        error() {
-          alert('Plz refresh the page, something went wrong');
-        },
-      });
+    return this.http.get(`${API}`).subscribe({
+      next(response) {
+        console.log(response);
+      },
+      error() {
+        alert('Plz refresh the page, something went wrong');
+      },
+    });
   }
 
   private basicAuth(
