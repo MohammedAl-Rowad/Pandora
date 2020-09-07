71d0ef862ef08ec94a8bff6e8aa8a78dcfd41407
MohammedAl-rowad <rowadwo@gmail.com>
2020-09-07 18:22:43 +0300

    #1

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { HomeComponent } from './components/home/home.component';
+import { MatModule } from './modules/mat/mat.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, DashboardModule],
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, DashboardModule, MatModule],
   providers: [],
   bootstrap: [AppComponent],
 })
