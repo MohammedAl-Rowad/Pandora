d32831ed55073d9c958319f2d22c300b1d48eaa0
MohammedAl-rowad <rowadwo@gmail.com>
2020-09-07 19:18:21 +0300

    #2

--- API/server.js
+++ API/server.js
@@ -3,13 +3,48 @@ const app = express()
 const cors = require('cors')
 const { default: axios } = require('axios')
 const port = 3000
-
+const endPoint = 'https://edraak.atlassian.net/rest/api/2'
+const { config } = require('dotenv')
+config()
 app.use(cors())
 
-app.get('/data', async (req, res, next) => {
-  const data = await axios.get().then(({ data }) => data)
+const { USERNAME, API_TOKEN } = process.env
 
-  res.json(data)
+app.get('/data', async (req, res, next) => {
+  try {
+    // const data = await axios
+    //   .get(,
+    //     'https://edraak.atlassian.net/rest/api/2/EC2020',
+    //     {
+    //       auth: {
+    //         username: USERNAME,
+    //         password: API_TOKEN // password
+    //       },
+    //     }
+    //   )
+    //   .then(({ data }) => data)
+    res.json(require('./data'))
+  } catch (error) {
+    console.error(error)
+    res.end()
+  }
+})
+
+app.get('/projects', async (req, res) => {
+  try {
+    const data = await axios
+      .get(`${endPoint}/project`, {
+        auth: {
+          username: USERNAME,
+          password: API_TOKEN, // password
+        },
+      })
+      .then(({ data }) => data)
+    res.json(data)
+  } catch (error) {
+    console.error(error)
+    res.end()
+  }
 })
 
 app.listen(port, () => {
