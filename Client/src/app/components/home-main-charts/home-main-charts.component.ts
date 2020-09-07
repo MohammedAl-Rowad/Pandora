ec8ebaae33f87e829457228db2c6bc5427b936ff
MohammedAl-rowad <rowadwo@gmail.com>
2020-09-07 18:49:15 +0300

    #1

---
+++ Client/src/app/components/home-main-charts/home-main-charts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pandora-home-main-charts',
+  templateUrl: './home-main-charts.component.html',
+  styleUrls: ['./home-main-charts.component.scss']
+})
+export class HomeMainChartsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- Client/src/app/components/home/home.component.ts
+++ Client/src/app/components/home/home.component.ts
@@ -1,19 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'pandora-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   breakpoint: number;
   constructor() {}
-
-  ngOnInit(): void {
-    this.breakpoint = window.innerWidth <= 400 ? 1 : 6;
-  }
-
-  onResize(event) {
-    this.breakpoint = event.target.innerWidth <= 400 ? 1 : 6;
-  }
 }
